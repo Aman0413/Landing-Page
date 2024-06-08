062532131d961233d64c8ad53e0198a3ba34ed61
Aman Verma <aman@Amans-MacBook-Air.local>
2024-06-08 14:00:34 +0530

    complete faq

--- src/app/components/FAQ/Faq.jsx
+++ src/app/components/FAQ/Faq.jsx
@@ -1,21 +1,81 @@
+import Image from "next/image";
 import React from "react";
+import img1 from "/public/Frame.png";
 
 function Faq() {
+  const data = [
+    {
+      id: 2,
+      title: "The Best Financial Accounting App Ever!",
+      description: `“Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+              ultricies. In ultrices malesuada elit mauris.`,
+    },
+    {
+      id: 1,
+      title: "The Best Financial Accounting App Ever!",
+      description: `“Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+              ultricies. In ultrices malesuada elit mauris.`,
+    },
+    {
+      id: 3,
+      title: "The Best Financial Accounting App Ever!",
+      description: `“Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+              ultricies. In ultrices malesuada elit mauris.`,
+    },
+    {
+      id: 4,
+      title: "The Best Financial Accounting App Ever!",
+      description: `“Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+              ultricies. In ultrices malesuada elit mauris.`,
+    },
+    {
+      id: 6,
+      title: "The Best Financial Accounting App Ever!",
+      description: `“Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+              ultricies. In ultrices malesuada elit mauris.`,
+    },
+    {
+      id: 5,
+      title: "The Best Financial Accounting App Ever!",
+      description: `“Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+              ultricies. In ultrices malesuada elit mauris.`,
+    },
+  ];
+
   return (
     <div>
-      <div>
-        <h1>FAQ</h1>
-        <h1>Frequently Asked Questions</h1>
-
+      <div className="flex flex-col space-y-6 ">
         <div>
-          <div>
-            <span>The Best Financial Accounting App Ever!</span>
-            <p>
-              “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
-              ultricies. In ultrices malesuada elit mauris.
-            </p>
-          </div>
+          <h2 className="text-orange-primary">FAQ</h2>
+          <h1 className="font-bold text-3xl ">Frequently Asked Questions</h1>
         </div>
+
+        <div className="flex gap-2 flex-wrap  ">
+          {data.map((item) => (
+            <div
+              key={item.id}
+              className={`
+                ${
+                  item.id % 2 === 0
+                    ? "bg-orange-primary text-white-primary"
+                    : "text-black-primary"
+                }
+                
+                 p-10 rounded-md  md:w-[48%] `}
+            >
+              <span className="font-semibold text-xl">
+                The Best Financial Accounting App Ever!
+              </span>
+              <p className="font-light">
+                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+                ultricies. In ultrices malesuada elit mauris.
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="pb-10  flex items-center justify-center">
+        <Image src={img1} alt="imgage" className="w-full" />
       </div>
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Navbar from "@/app/components/Navbar/Navbar";
 import HeroSection from "./components/HeroSection/HeroSection";
 import Footer from "./components/Footer/Footer";
+import Faq from "./components/FAQ/Faq";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <div className="w-[90%] p-2">
         <Navbar />
         <HeroSection />
+        <Faq />
         <Footer />
       </div>
     </main>
